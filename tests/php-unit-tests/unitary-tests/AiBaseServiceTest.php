f4a921a0c2bcb6c8f9eb9aba49288e06a858ee27
jf-cbd <jimmy.fayolle@combodo.com>
2025-06-18 11:58:32 +0200

    :white_check_mark: add tests

---
+++ tests/php-unit-tests/unitary-tests/AiBaseServiceTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+namespace Itomig\iTop\AiBase\Test;
+
+use Combodo\iTop\Test\UnitTest\ItopTestCase;
+use Itomig\iTop\Extension\AIBase\Engine\iAIEngineInterface;
+use Itomig\iTop\Extension\AIBase\Service\AIService;
+
+class AiBaseServiceTest extends ItopTestCase
+{
+	protected function setUp(): void
+	{
+		parent::setUp();
+		$this->RequireOnceItopFile('/env-production/itomig-ai-base/vendor/autoload.php');
+	}
+
+	public function testGetCompletion(): void
+	{
+		$oAIEngineInterfaceMock = $this->createMock(iAIEngineInterface::class);
+		$oAIEngineInterfaceMock->expects(static::once())
+			->method('getCompletion')
+			->willReturn('<think>Here is my reasoning</think>Mocked response');
+
+		$oAIService = new AIService($oAIEngineInterfaceMock);
+		$sCompletion = $oAIService->GetCompletion('This is a test prompt', 'ok');
+		static::assertEquals('Mocked response', $sCompletion); // no more think tag, cleaned up by AIService::GetCompletion
+	}
+}
