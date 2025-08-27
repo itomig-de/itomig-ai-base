2fde5ce6f8907c0a8c6aedf7005b9836c54ebf93
Lars Kaltefleiter <lars.kaltefleiter@itomig.de>
2025-08-27 12:47:33 +0200

    Bump module version to 25.3.1

--- module.itomig-ai-base.php
+++ module.itomig-ai-base.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itomig-ai-base/25.3.0',
+	'itomig-ai-base/25.3.1',
 	array(
 		// Identification
 		//
