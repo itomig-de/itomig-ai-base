fd4fce39e7cc4709c028b5fb0dace6618681f52c
jf-cbd <jf-cbd@users.noreply.github.com>
2026-06-17 16:35:48 +0200

    Dump autoloader
    (cherry picked from commit 4c35b29eb8d3ed184658d0951262f635228945e1)

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -11,5 +11,4 @@ return array(
     '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
     '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
     '61ba3e92ef91a7a0f1cc260ebd9290e4' => $vendorDir . '/openai-php/client/src/OpenAI.php',
-    '9b38cf48e83f5d8f60375221cd213eee' => $vendorDir . '/phpstan/phpstan/bootstrap.php',
 );
